8fd860f588b7d8674e590f255e7b01aa79ec27ef
Marius Thingwall <marius.thingwall@gmail.com>
2015-06-18 09:31:39 +0200

    fjas

--- TwitterJava/src/main.java
+++ TwitterJava/src/main.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import twitter4j.conf.*;
 
+
+//This program requires an OAuth property file for access to the twitter API.
+
 public class main {
 
     private static int pageNumber = 1;
-    private static String user = "Senjinx";
-    public static List<Status> statuses = new ArrayList<>();
+    private static String user = "Nettkvalitet";
+    private static List<Status> statuses = new ArrayList<>();
 
 
     public static void main(String[] args) throws Exception {
@@ -55,7 +58,7 @@ public class main {
 
 
 
-//Old shit
+//Old code used for interacting with the API
 /*    private static int counter = 0;
     private static int pageNumber = 1;
 
@@ -69,7 +72,6 @@ public class main {
 
         //post the last 20 tweets on any given feed.
         Twitter twitter = TwitterFactory.getSingleton();
-
 
         List<Status> statuses = twitter.getUserTimeline("Nettkvalitet");
 
