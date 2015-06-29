26d98e041882a33ab59c9aaa15af9557c44e7ce5
Tjohei <gmalethe@gmail.com>
2015-06-29 12:43:17 +0200

    Lagde klasse som skal  hente og forberede info fra elasticsearch.

--- spraak/src/main/java/documentWriter/PDFWriter.java
+++ spraak/src/main/java/documentWriter/PDFWriter.java
@@ -14,100 +14,91 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by camp-lsa on 26.06.2015.
  */
 public class PDFWriter {
-    public PDFWriter(){
-        PDDocument doc = new PDDocument();
-        PDFont font = PDType1Font.TIMES_ROMAN;
-        try{
-            PDPage page = new PDPage();
-            doc.addPage(page);
 
-            PDRectangle box = page.findMediaBox();
-            float margin = 72;
-            float width = box.getWidth()-margin-margin;
-            float startX = box.getLowerLeftX() + margin;
-            float startY = box.getUpperRightY()-margin;
-            PDPageContentStream contents = new PDPageContentStream(doc,page);
-            contents.beginText();
-                contents.setFont(font,20);
-                contents.moveTextPositionByAmount(startX,startY);
-                contents.drawString("Yoyoyoyo");
-                contents.endText();
-            contents.close();
-            doc.save("C:\\Users\\camp-lsa\\Documents\\"+ System.currentTimeMillis() +".pdf");
-            doc.close();
+    public final static String outputPath = "C:\\Users\\camp-lsa\\latex";
+    public static String report = "";
 
-        }catch(Exception e) {
-            e.printStackTrace();
+    final static String header =""+
+            "\\documentclass[pdflatex]{article}\n " +
+            "\\usepackage[latin1]{inputenc}\n  " +
+            "\\usepackage{changepage}\n"+
+            "\\usepackage{graphicx}\n"+
+            "\\begin{document}\n";
+    final static String footer = " \\end{document}";
+    final static String newLine = "\\newline \n";
+
+    public static void main(String... args){
+        Map<String, Map> m = new LinkedHashMap<String, Map>();
+        Map<String, Map> m1 = new LinkedHashMap<String, Map>();
+        Map<String, String> n = new LinkedHashMap<String, String>();
+        n.put("Mengde Nynorsk","20%");
+        n.put("Mengde Bokmål","80%");
+        n.put("Kompleksitet Nynorsk","37 LIX");
+        n.put("Kompleksitet Bokmål","38 LIX");
+        m1.put("Norge.no", n);
+        m.put("Side", m1);
+
+        m1 = new LinkedHashMap<String, Map>();
+        n = new LinkedHashMap<String, String>();
+        n.put("Mengde Nynorsk","40%");
+        n.put("Mengde Bokmål","60%");
+        n.put("Kompleksitet Nynorsk","15 LIX");
+        n.put("Kompleksitet Bokmål","14 LIX");
+        m1.put("twitter.no/difi", n);
+        m.put("Sosiale Medier", m1);
+        setMap(m,0);
+        getReport();
+    }
+    public static String getSectionType(int depth, String title){
+        String sub = "";
+        for(int i = 0; i < depth; i++)
+            sub += "sub";
+        return "\\"+sub+"section{"+title+"}";
+    }
+    public static void setMap(Map contents, int depth) {
+        report +="\\begin{adjustwidth}{"+depth+"em}{0pt}";
+        for (Object s : contents.keySet()){
+            if (contents.get(s) instanceof HashMap) {
+
+                report += getSectionType(depth,(String)s)+"\n";
+
+                setMap((HashMap<String, Object>) contents.get(s),depth+1);
+            } else {
+                String segment = ((String) contents.get(s)).replace("å", "{\\aa}").replace("%", "\\%");
+                report += "\\textbf{" + ((String) s).replace("å", "{\\aa}") + "}: " + segment + newLine;
+            }
         }
+        report +="\\end{adjustwidth}";
     }
 
-    static String header = "\\documentclass[pdflatex]{article}\n " +
-            "\\usepackage[latin1]{inputenc}  " +
-            //"\\usepackage[utf8]{inputenc} " +
-            "\\usepackage{graphicx}" +
-            "\\begin{document}";
-    static String footer = " \\end{document}";
-    static String newLine = "\\newline \n";
+
     private static long lastWritten = -1;
-    public static String getReport(HashMap<String,String> content){
-        //TODO: bestem syntaks for content og skriv ut det i stedet.
-
-        long time = System.currentTimeMillis();
-        if(lastWritten != -1 && time-lastWritten < 20000) {
-            System.out.println("Time since last write is below 20s");
-            return "C:\\users\\camp-lsa\\test\\" + lastWritten + ".pdf";
-        }
+    public static String getReport() {
         try {
-            for(String key : content.keySet()){
-
-            }
-
+            long time = System.currentTimeMillis();
             String document = header;
-            document += "\\textbf{Statistikk for bruk av nynorsk og bokm{\\aa}l p{\\aa} statlige sider:} " + newLine;
-            document += "Tekster p{\\aa} nynorsk:  " + MainClass.nn + newLine;
-            document += "Tekster p{\\aa} Bokm{\\aa}l: " + MainClass.nb + newLine;
-            document += newLine;
-            document += "Kompleksitet Nynorsk: " + ((int) (MainClass.total_nn_complexity /(MainClass.nn == 0 ? 1 : MainClass.nn))) +newLine;
-            document += "Kompleksitet Bokm{\\aa}l: " + ((int) (MainClass.total_nb_complexity / (MainClass.nb == 0 ? 1 : MainClass.nb))) +newLine;
-            document += "\\begin{figure}[b!p]\\centering\\includegraphics[scale=0.2]{bilde}\\end{figure}";
+            document += report;
             document += footer;
-            document.replaceAll("å","{\\aa}");
-            String command = "cmd.exe /c cd C:\\Users\\camp-lsa\\test & pdflatex \""+ document + "\" -job-name="+time + " -include-directory=C:\\Users\\camp-lsa\\latex";
-            System.out.println(command.replaceAll("\n",""));
-            System.out.println();
-
-
+            System.out.println(header);
+            System.out.println(report);
+            String command = "cmd.exe /c cd C:\\Users\\camp-lsa\\test & pdflatex \""+ document + "\" -job-name="+ time + " -include-directory="+outputPath;  //+" -quiet -disable-installer"
+            System.out.println("\n\n WRITING \n****************************************");
             Process cmd = Runtime.getRuntime().exec(command);
             BufferedReader br = new BufferedReader(new InputStreamReader(cmd.getInputStream()));
-            String line ="";
-            while((line = br.readLine()) != null){
+            String line = "";
+            while((line = br.readLine()) != null)
                 System.out.println(line);
-            }
             br.close();
-            lastWritten =time;
-            return "C:\\users\\camp-lsa\\test\\"+time+".pdf";
-            /*
-            Scanner c = new Scanner(cmd.getInputStream());
-            cmd.getOutputStream().write("cd C:\\Users\\camp-lsa\\test \r\n".getBytes());
-            cmd.getOutputStream().flush();
-            cmd.getOutputStream().write("dir > o.txt \r\n".getBytes());
-            cmd.getOutputStream().flush();*/
+            return outputPath+"\\"+time+".pdf";
         }catch(Exception e){
             e.printStackTrace();
             return null;
         }
-    }
-    public static void main(String... args){
-
-        System.out.println("Writing pdf...");
-        getReport(null);
-
     }
 }
