5a8e51c91e1ac66fbf08789f6ad941e65d9bcf55
Marius Thingwall <marius.thingwall@gmail.com>
2015-06-30 12:12:39 +0200

    added lix chart

--- spraak_frontend/public/javascripts/chart.js
+++ spraak_frontend/public/javascripts/chart.js
@@ -1,30 +1,59 @@
-
 var bokmal;
 var nynorsk;
-var nnPercent;
-var nbPercent ;
+var nnPercentAll;
+var nbPercentAll;
+var nnComplex = [];
+var nbComplex = [];
 
-$.getJSON('http://localhost:3002/api/all',function(data) {
+$.getJSON('http://localhost:3002/api/v1/all',function(data) {
 
-    $.each(data.lang_terms, function() {
-        $.each(this, function(k, v) {
-            if (v.key === "nb") {
-                bokmal = v.doc_count;
-            } else {
-                nynorsk = v.doc_count;
 
-            };
-        });
-    });
+    //Gets total bokmål and nynorsk data.
+    bokmal = data.all.lang_terms.buckets[0].doc_count;
+    nynorsk = data.all.lang_terms.buckets[1].doc_count;
     var nb = parseInt(bokmal);
     var nn = parseInt(nynorsk);
-    nnPercent = (nn / (nb + nn))*100;
-    nbPercent = (nb / (nb + nn))*100;
+    nnPercentAll = (nn / (nb + nn))*100;
+    nbPercentAll = (nb / (nb + nn))*100;
 
 
-    console.log(nbPercent);
-    console.log(nnPercent);
 
+    var format = function(v){
+
+
+        if (v.complexity_nn.doc_count > 0){
+
+            console.log(v.complexity_nn.complexity.avg);
+            var nn = parseInt(v.complexity_nn.complexity.avg);
+            nnComplex.push(nn);
+
+
+        } else {
+            nnComplex.push(0);
+        }
+
+        if  (v.complexity_nb.doc_count > 0) {
+            console.log(v.complexity_nb.complexity.avg);
+            var nb = parseInt(v.complexity_nb.complexity.avg)
+            nbComplex.push(nb);
+
+
+        }
+        else {
+            nbComplex.push(0);
+
+        }
+    };
+
+
+      $.each(data.toptags.buckets, function () {
+
+
+          format(this);
+      });
+
+    console.log(nbComplex);
+    console.log(nnComplex);
 
 
     $('#piechart').highcharts({
@@ -58,18 +87,81 @@ $.getJSON('http://localhost:3002/api/all',function(data) {
             colorByPoint: true,
             data: [{
                 name: "Nynorsk",
-                y: nnPercent,
+                y: nnPercentAll,
                 sliced: true,
                 selected: true
 
             }, {
                 name: "Bokmål",
-                y: nbPercent
+                y: nbPercentAll
 
 
             }]
         }]
     });
+
+
+        $('#lixChart').highcharts({
+            chart: {
+                type: 'areaspline'
+            },
+            title: {
+                text: 'Gjennomsnittlig kompleksitetsgrad nynorsk og bokmål'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'left',
+                verticalAlign: 'top',
+                x: 570,
+                y: 60,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            },
+            xAxis: {
+                categories: [
+                    'Web',
+                    'Twitter',
+                    'Facebook',
+                    'Docx',
+                    'PDF',
+                    'Doc',
+                    'ODT'
+
+                ]
+
+            },
+            yAxis: {
+                title: {
+                    text: 'LIX-score'
+                }
+            },
+            tooltip: {
+                shared: true,
+                valueSuffix: ''
+            },
+            credits: {
+                enabled: false
+            },
+            plotOptions: {
+                areaspline: {
+                    fillOpacity: 0.5
+                }
+            },
+            series: [{
+                name: 'Nynorsk',
+                data: nnComplex
+            }, {
+                name: 'Bokmål',
+                data: nbComplex
+            }]
+        });
+
+
+
 });
+
+
+
 
 
