3bce010f19dc0f437583404604ce53254c58d3e4
Morten Linderud <Morten@linderud.pw>
2015-06-30 09:24:40 +0200

    Added owners api

--- spraak_frontend/routes/api.js
+++ spraak_frontend/routes/api.js
@@ -259,7 +259,35 @@ router.get("/v1/all/:type", (function(req, res) {
     });
 }))
 
-router.get("/v1/all/owner/:owner", (function(req, res) {
+router.get("/v1/owners", (function(req, res) {
+    client.search({
+        index: 'spraak',
+        body: {
+            aggs: {
+                toptags: {
+                    terms: {
+                        field: "owner"
+                    },
+                    aggs: {
+                        lang_terms: {
+                            terms: {
+                                field: "lang"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }).then(function (resp) {
+        console.log(resp.aggregations);
+        var hits = resp.aggregations;
+        res.send(hits);
+    }, function (err) {
+        console.trace(err.message);
+    });
+}))
+
+router.get("/v1/owner/:owner", (function(req, res) {
     client.search({
         index: 'spraak',
         body: {
