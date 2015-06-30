ebc3de06bf0568e34f1e0d4e387d0e46e9ab4c2b
Morten Linderud <Morten@linderud.pw>
2015-06-30 13:02:03 +0200

    API v2

--- spraak_frontend/routes/api.js
+++ spraak_frontend/routes/api.js
@@ -178,4 +178,90 @@ router.get("/v1/owner/:owner", (function(req, res) {
     });
 }))
 
+
+
+router.get("/v2/owner/:owner/all", (function(req, res) {
+    client.search({
+        index: 'spraak',
+        body: {
+            query: {
+                filtered: {
+                    query: {match_all: {}},
+                    filter:{
+                        term: {owner: req.params.owner}
+                    }
+                }
+            },
+            aggs: {
+                toptags: {
+                    terms: {
+                        field: "type"
+                    },
+                    aggs: all
+                }
+            }
+        }
+    }).then(function (resp) {
+        console.log(resp.aggregations);
+        var hits = resp.aggregations;
+        res.send(hits);
+    }, function (err) {
+        console.trace(err.message);
+    });
+}))
+
+router.get("/v2/owners", (function(req, res) {
+    client.search({
+        index: 'spraak',
+        body: {
+            aggs: {
+                toptags: {
+                    terms: {
+                        field: "owner"
+                    },
+                }
+            }
+        }
+    }).then(function (resp) {
+        var hits = resp.aggregations;
+        var d = resp.aggregations.toptags.buckets;
+        var l = [];
+        for(var i in d){
+            console.log(d[i])
+            l.push(d[i].key)
+        }
+        res.send(l);
+    }, function (err) {
+        console.trace(err.message);
+    });
+}))
+
+router.get("/v2/owners/lang", (function(req, res) {
+    client.search({
+        index: 'spraak',
+        body: {
+            aggs: {
+                toptags: {
+                    terms: {
+                        field: "owner"
+                    },
+                    aggs: {
+                        lang_terms: {
+                            terms: {
+                                field: "lang"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }).then(function (resp) {
+        console.log(resp.aggregations);
+        var hits = resp.aggregations;
+        res.send(hits);
+    }, function (err) {
+        console.trace(err.message);
+    });
+}))
+
 module.exports = router;
