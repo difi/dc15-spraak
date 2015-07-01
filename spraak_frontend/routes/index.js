621054d72d0b659c4d5996ca75753e1b57e88de4
Marius Thingwall <marius.thingwall@gmail.com>
2015-07-01 13:26:31 +0200

    fixed multipage support, menu ++

--- spraak_frontend/routes/index.js
+++ spraak_frontend/routes/index.js
@@ -10,5 +10,16 @@ router.get('/nn', function(req, res, next) {
   res.render('index_nn', { title: 'Express' });
 });
 
+router.get('/complex', function(req, res, next) {
+  res.render('complex', { title: 'Express' });
+});
+
+router.get('/total', function(req, res, next) {
+  res.render('total', { title: 'Express' });
+});
+
+router.get('/agency', function(req, res, next) {
+  res.render('agency', { title: 'Express' });
+});
 
 module.exports = router;
