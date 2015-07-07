b7c3544eec7afae5972b420347e017c468e26cf1
Marius Thingwall <marius.thingwall@gmail.com>
2015-07-07 12:49:42 +0200

    added some fine tries at jquery.

--- spraak_frontend/public/javascripts/activeButton.js
+++ spraak_frontend/public/javascripts/activeButton.js
@@ -1,38 +1,132 @@
-/*$(function(){
-
-    $('.pure-menu-link a').click(function(){
-
-        $('.pure-menu-link.active').removeClass('active');
-        $(this).addClass('active');
-
-    });
-
-});*/
-
 /*
-$(function(){
-    $('.pure-menu-link .active a').filter(function(){return this.href==location.href}).parent().addClass('active').siblings().removeClass('active')
-    $('.pure-menu-link .active a').click(function(){
-        $(this).parent().addClass('active').siblings().removeClass('active')
-    })
-});
-*/
-/*
-
-
-$( "#result" ).load( "ajax/test.html #container" , function () {
-
-});*/
-
-/*
-
 
 $(document).ready(function(){
-    $('.total').click(function(){
-        $('.contentLoad').load('html/total.html');
+
+
+    $("#difi").click(function(){
+        $("#main").hide();
+        $("#total").show();
+    });
+    $("#home").click(function(){
+        $("#main").show();
+        $("#total").hide();
     });
 });
+
+
+
 */
+
+/*
+
+$('#difi').click( function() {
+$.ajax({ url: 'localhost:3002/total.html'
+    , type: 'GET'
+    , dataType: 'html'
+})
+    .done(function(data) {
+        $('#container').html(data);
+        piechart();
+
+    })
+    .fail(function() {
+        console.log("Something went wrong!");
+    });
+
+    return false; } );
+
+
+
+$('#complex').click( function() {
+    $.ajax({ url: 'html/complex.html'
+        , type: 'GET'
+        , dataType: 'html'
+    })
+        .done(function(data) {
+            $('#container').html(data);
+        })
+        .fail(function() {
+            console.log("Something went wrong!");
+        });
+
+    return false; } );
+
+
+
+$('#agency').click( function() {
+    $.ajax({ url: 'html/agency.html'
+        , type: 'GET'
+        , dataType: 'html'
+    })
+        .done(function(data) {
+            $('#container').html(data);
+        })
+        .fail(function() {
+            console.log("Something went wrong!");
+        });
+
+    return false; } );
+
+$('#home').click( function() {
+
+    $.ajax({ url: 'html/index.html'
+        , type: 'GET'
+        , dataType: 'html'
+    })
+        .done(function(data) {
+            $('#container').html(data);
+        })
+        .fail(function() {
+            console.log("Something went wrong!");
+        });
+
+    return false; } );
+
+
+*/
+
+
+
+
+
+
+
+
+/*$(function(){
+
+ $('.pure-menu-link a').click(function(){
+
+ $('.pure-menu-link.active').removeClass('active');
+ $(this).addClass('active');
+
+ });
+
+ });*/
+
+/*
+ $(function(){
+ $('.pure-menu-link .active a').filter(function(){return this.href==location.href}).parent().addClass('active').siblings().removeClass('active')
+ $('.pure-menu-link .active a').click(function(){
+ $(this).parent().addClass('active').siblings().removeClass('active')
+ })
+ });
+ */
+/*
+
+
+ $( "#result" ).load( "ajax/test.html #container" , function () {
+
+ });*/
+
+/*
+
+
+ $(document).ready(function(){
+ $('.total').click(function(){
+ $('.contentLoad').load('html/total.html');
+ });
+ });
+ */
 
 
 //$( ".contentLoad" ).load( "html/total.html", function() {
--- spraak_frontend/public/javascripts/chart.js
+++ spraak_frontend/public/javascripts/chart.js
@@ -11,7 +11,11 @@ var drilldown_series = [];
 var data_list = [];
 
 
+
+
+
 $.getJSON('http://localhost:3002/api/v2/owner/'+user+'/all',function(data) {
+
 
 
     //Gets total bokmål and nynorsk data.
@@ -19,14 +23,13 @@ $.getJSON('http://localhost:3002/api/v2/owner/'+user+'/all',function(data) {
     nynorsk = data.all.lang_terms.buckets[1].doc_count;
     var nb = parseInt(bokmal);
     var nn = parseInt(nynorsk);
-    nnPercentAll = (nn / (nb + nn))*100;
-    nbPercentAll = (nb / (nb + nn))*100;
+    nnPercentAll = (nn / (nb + nn)) * 100;
+    nbPercentAll = (nb / (nb + nn)) * 100;
 
 
-
-    var format = function(v){
-        if (v.complexity_nn.doc_count > 0){
-            if(v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt") {
+    var format = function (v) {
+        if (v.complexity_nn.doc_count > 0) {
+            if (v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt") {
                 a += parseInt(v.complexity_nn.complexity.avg);
 
             } else {
@@ -35,23 +38,23 @@ $.getJSON('http://localhost:3002/api/v2/owner/'+user+'/all',function(data) {
 
             }
         } else {
-            if(v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt") {
-                a+= 0;
+            if (v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt") {
+                a += 0;
             } else {
                 nnComplex.push(0);
             }
         }
 
-        if  (v.complexity_nb.doc_count > 0) {
-            if((v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt")){
+        if (v.complexity_nb.doc_count > 0) {
+            if ((v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt")) {
                 b += parseInt(v.complexity_nb.complexity.avg);
             } else {
                 var nb = parseInt(v.complexity_nb.complexity.avg);
                 nbComplex.push(nb);
             }
         } else {
-            if(v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt") {
-                b+= 0;
+            if (v.key === "docx" || v.key === "doc" || v.key === "pdf" || v.key === "odt") {
+                b += 0;
             }
             else {
                 nbComplex.push(0);
@@ -60,57 +63,57 @@ $.getJSON('http://localhost:3002/api/v2/owner/'+user+'/all',function(data) {
     };
 
     $.each(data.toptags.buckets, function () {
-          format(this);
+        format(this);
     });
 
-    nnComplex.push(a/4);
-    nbComplex.push(b/4);
+    nnComplex.push(a / 4);
+    nbComplex.push(b / 4);
 
-
-
-    $('#piechart').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Nynorsk- og bokmålsandelen til Difi:'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+        $('#piechart').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Nynorsk- og bokmålsandelen til Difi:'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
                     }
                 }
-            }
-        },
-        series: [{
-            name: "Andel",
-            colorByPoint: true,
-            data: [{
-                name: "Nynorsk",
-                y: nnPercentAll,
-                sliced: true,
-                selected: true
+            },
+            series: [{
+                name: "Andel",
+                colorByPoint: true,
+                data: [{
+                    name: "Nynorsk",
+                    y: nnPercentAll,
+                    sliced: true,
+                    selected: true
 
-            }, {
-                name: "Bokmål",
-                y: nbPercentAll
+                }, {
+                    name: "Bokmål",
+                    y: nbPercentAll
 
 
+                }]
             }]
-        }]
-    });
+        });
+
+
 
 
         $('#lixChart').highcharts({
