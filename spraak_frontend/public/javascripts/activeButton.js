cba14c866f00eccc1212842eb929c02e8ed6644e
Marius Thingwall <marius.thingwall@gmail.com>
2015-07-08 14:39:30 +0200

    removed comments

--- spraak_frontend/public/javascripts/activeButton.js
+++ spraak_frontend/public/javascripts/activeButton.js
@@ -1,29 +1,4 @@
-/*
 
-$("#home").on('click', function () {
-    $("#difi").removeClass("active");
-    $("#complex").removeClass("active");
-    $("#agency").removeClass("active");
-
-    $(document).ready(function() {
-
-
-
-    });
-});
-
-
-$("#difi").on('click', function () {
-    $("#home").removeClass("active");
-    $("#complex").removeClass("active");
-    $("#agency").removeClass("active");
-
-    $(document).ready(function() {
-
-        $("#difi").addClass("active");
-
-    });
-});*/
 
 
 var url = window.location.href;
@@ -40,45 +15,3 @@ $(document).ready(function() {
     }
 });
 
-
-
-
-/*$(function(){
-
- $('.pure-menu-link a').click(function(){
-
- $('.pure-menu-link.active').removeClass('active');
- $(this).addClass('active');
-
- });
-
- });*/
-
-/*
- $(function(){
- $('.pure-menu-link .active a').filter(function(){return this.href==location.href}).parent().addClass('active').siblings().removeClass('active')
- $('.pure-menu-link .active a').click(function(){
- $(this).parent().addClass('active').siblings().removeClass('active')
- })
- });
- */
-/*
-
-
- $( "#result" ).load( "ajax/test.html #container" , function () {
-
- });*/
-
-/*
-
-
- $(document).ready(function(){
- $('.total').click(function(){
- $('.contentLoad').load('html/total.html');
- });
- });
- */
-
-
-//$( ".contentLoad" ).load( "html/total.html", function() {
-//});
