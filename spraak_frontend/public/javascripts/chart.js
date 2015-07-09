9b34b6e16da202fe28f4c9f9dda84b564d86e07f
Marius Thingwall <marius.thingwall@gmail.com>
2015-07-09 11:59:13 +0200

    first attempt at multiselect menu

--- spraak_frontend/public/javascripts/chart.js
+++ spraak_frontend/public/javascripts/chart.js
@@ -8,160 +8,159 @@ var user = "difi";
 var drilldown_series = [];
 var data_list = [];
 var toptags = [];
+var url = window.location.href;
 
 
+if (url === "http://localhost:3002/total" || url === "http://localhost:3002/complex") {
+    $.getJSON('http://localhost:3002/api/v3/owner/' + user + '/all', function (data) {
+
+        /*
+         //Gets total bokmål and nynorsk data.
+         bokmal = data.all.lang_terms.buckets[0].doc_count;
+         nynorsk = data.all.lang_terms.buckets[1].doc_count;
+         var nb = parseInt(bokmal);
+         var nn = parseInt(nynorsk);
+         nnPercentAll = (nn / (nb + nn)) * 100;
+         nbPercentAll = (nb / (nb + nn)) * 100;
+         */
+        var format = function (v) {
+
+            if (v.complexity_nn.doc_count > 0) {
+                var nn = parseInt(v.complexity_nn.avg);
+                nnComplex.push(nn);
+            } else {
+                nnComplex.push(0);
+            }
+            if (v.complexity_nb.doc_count > 0) {
+
+                var nb = parseInt(v.complexity_nb.avg);
+                nbComplex.push(nb);
+            } else {
+                nbComplex.push(0);
+            }
+
+        };
 
 
-$.getJSON('http://localhost:3002/api/v3/owner/'+user+'/all',function(data) {
+        $.each(data.toptags, function (key) {
+            format(this);
+
+            if (key === "web") {
+                toptags.push("Web");
+            } else if (key === "file") {
+                toptags.push("Documents")
+            } else if (key === "fb") {
+                toptags.push("Facebook")
+            } else if (key === "twitter") {
+                toptags.push("Twitter")
+            }
 
 
+        });
+        if (url === "http://localhost:3002/total") {
+            console.log("piechart loaded");
 
-/*
-    //Gets total bokmål and nynorsk data.
-    bokmal = data.all.lang_terms.buckets[0].doc_count;
-    nynorsk = data.all.lang_terms.buckets[1].doc_count;
-    var nb = parseInt(bokmal);
-    var nn = parseInt(nynorsk);
-    nnPercentAll = (nn / (nb + nn)) * 100;
-    nbPercentAll = (nb / (nb + nn)) * 100;
-*/
-
-
-
-
-    var format = function(v){
-
-        if (v.complexity_nn.doc_count > 0)  {
-            var nn = parseInt(v.complexity_nn.avg);
-            nnComplex.push(nn);
-        } else {
-            nnComplex.push(0);
-        }
-        if  (v.complexity_nb.doc_count > 0) {
-
-            var nb = parseInt(v.complexity_nb.avg);
-            nbComplex.push(nb);
-        } else {
-            nbComplex.push(0);
-        }
-
-    };
-
-
-
-    $.each(data.toptags, function (key) {
-        format(this);
-
-        if (key === "web") {
-            toptags.push("Web");
-        } else if (key === "file") {
-            toptags.push("Documents")
-        } else if (key === "fb") {
-            toptags.push("Facebook")
-        } else if (key === "twitter") {
-            toptags.push("Twitter")
-        }
-
-
-    });
-
-
-
-        $('#piechart').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Nynorsk- og bokmålsandelen til Difi:'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                        style: {
-                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+            $('#piechart').highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Nynorsk- og bokmålsandelen til Difi:'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
                         }
                     }
-                }
-            },
-            series: [{
-                name: "Andel",
-                colorByPoint: true,
-                data: [{
-                    name: "Nynorsk",
-                    y: nnPercentAll,
-                    sliced: true,
-                    selected: true
+                },
+                series: [{
+                    name: "Andel",
+                    colorByPoint: true,
+                    data: [{
+                        name: "Nynorsk",
+                        y: 25,
+                        sliced: true,
+                        selected: true
 
-                }, {
-                    name: "Bokmål",
-                    y: nbPercentAll
+                    }, {
+                        name: "Bokmål",
+                        y: 75
 
 
+                    }]
                 }]
-            }]
-        });
+            });
 
+        }
 
+        if (url === "http://localhost:3002/complex") {
+            console.log("complex loaded");
 
-
-        $('#lixChart').highcharts({
-            chart: {
-                type: 'areaspline'
-            },
-            title: {
-                text: 'Gjennomsnittlig kompleksitetsgrad nynorsk og bokmål'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                verticalAlign: 'top',
-                x: 570,
-                y: 60,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-            },
-            xAxis: {
-                categories: toptags
-
-            },
-            yAxis: {
+            $('#lixChart').highcharts({
+                chart: {
+                    type: 'areaspline'
+                },
                 title: {
-                    text: 'LIX-score'
-                }
-            },
-            tooltip: {
-                shared: true,
-                valueSuffix: ''
-            },
-            credits: {
-                enabled: false
-            },
-            plotOptions: {
-                areaspline: {
-                    fillOpacity: 0.1
-                }
-            },
-            series: [{
-                name: 'Nynorsk',
-                data: nnComplex
-            }, {
-                name: 'Bokmål',
-                data: nbComplex
-            }]
-        });
-});
+                    text: 'Gjennomsnittlig kompleksitetsgrad nynorsk og bokmål'
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'left',
+                    verticalAlign: 'top',
+                    x: 570,
+                    y: 60,
+                    floating: true,
+                    borderWidth: 1,
+                    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+                },
+                xAxis: {
+                    categories: toptags
 
+                },
+                yAxis: {
+                    title: {
+                        text: 'LIX-score'
+                    }
+                },
+                tooltip: {
+                    shared: true,
+                    valueSuffix: ''
+                },
+                credits: {
+                    enabled: false
+                },
+                plotOptions: {
+                    areaspline: {
+                        fillOpacity: 0.1
+                    }
+                },
+                series: [{
+                    name: 'Nynorsk',
+                    data: nnComplex
+                }, {
+                    name: 'Bokmål',
+                    data: nbComplex
+                }]
+            });
+        }
+    });
+}
+
+
+if (url === "http://localhost:3002/agency") {
 $.getJSON('http://localhost:3002/api/v2/owners', function(data) {
     ownerList = data;
     var completedCalls = 0;
@@ -250,6 +249,8 @@ function drawAllOwnersChart() {
         }
     });
 }
+}
+
 
 
 // This is the version that will work with v3/owners/all eventually
