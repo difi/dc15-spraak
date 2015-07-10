a9c1701daeddfc43a9cacc626bee771a06fb8abb
Tjohei <gmalethe@gmail.com>
2015-07-10 09:04:44 +0200

    test

--- spraak/src/main/java/mongo.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by camp-lsa on 08.07.2015.
- */
-public class mongo {
-    public static void main(String... args){
-        String name = "hæi på døi";
-        System.out.println(name.replaceAll("å","a"));
-    }
-}
