b854655d823a5945dcfd303cffd0296c4a40bee5
Anna Kastet <annakastet@gmail.com>
2015-07-15 13:28:36 +0200

    Make menu mobile-friendly
    mobileMenu.js is copypasted from the difi.no scripts, and afffects other
    things than the menu.

--- spraak_frontend/public/javascripts/activeButton.js
+++ spraak_frontend/public/javascripts/activeButton.js
@@ -5,24 +5,24 @@ var url = window.location.href;
 
 $(document).ready(function() {
     if(url === "http://localhost:3002/") {
-        $("#home").addClass("active-trail");
+        $("#home").addClass("active-trail active");
     } else if (url === "http://localhost:3002/total") {
-        $("#difi").addClass("active-trail");
+        $("#difi").addClass("active-trail active");
     } else if (url === "http://localhost:3002/complex") {
-        $("#complex").addClass("active-trail");
+        $("#complex").addClass("active-trail active");
     } else if (url === "http://localhost:3002/agency") {
-        $("#agency").addClass("active-trail");
+        $("#agency").addClass("active-trail active");
     }
 
     //Same for nynorsk
     if(url === "http://localhost:3002/nn") {
-        $("#home").addClass("active-trail");
+        $("#home").addClass("active-trail active");
     } else if (url === "http://localhost:3002/total_nn") {
-        $("#difi").addClass("active-trail");
+        $("#difi").addClass("active-trail active");
     } else if (url === "http://localhost:3002/complex_nn") {
-        $("#complex").addClass("active-trail");
+        $("#complex").addClass("active-trail active");
     } else if (url === "http://localhost:3002/agency_nn") {
-        $("#agency").addClass("active-trail");
+        $("#agency").addClass("active-trail active");
     }
 });
 
