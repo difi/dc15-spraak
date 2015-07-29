8919ec12799f63f0c633ad75a9757a10d78d102e
Anna Kastet <annakastet@gmail.com>
2015-07-29 13:38:29 +0200

    Fix indentation and add newline at end of file

--- spraak_frontend/public/javascripts/wordCount.js
+++ spraak_frontend/public/javascripts/wordCount.js
@@ -1,74 +1,54 @@
 /*
-    Affects ordbruk.html and ordbruk_nn.html
+ Affects ordbruk.html and ordbruk_nn.html
  */
 if(url == "/ordbruk") {
-    $('#wordSearchButton').attr('disabled', 'disabled');
+    var wordSearchButton = $('#wordSearchButton');
+    var wordOwnersList = $('#wordOwnersList');
     var word = '';
-
+    wordSearchButton.attr('disabled', 'disabled');
     $('#wordSearch').keyup(function(event) {
         word = $('#wordSearch').val();
 
         /*
-            Enter-button while in text field == click on wordSearchButton.
+         Enter-button while in text field == click on wordSearchButton.
          */
         if(event.keyCode == 13) {
-            $("#wordSearchButton").click();
+            wordSearchButton.click();
         }
 
         /*
-            Disable wordSearchButton if search field is empty.
+         Disable wordSearchButton if search field is empty.
          */
         if(word == '') {
-            $('#wordSearchButton').attr('disabled', 'disabled');
+            wordSearchButton.attr('disabled', 'disabled');
         }
         else {
-            $('#wordSearchButton').removeAttr('disabled');
+            wordSearchButton.removeAttr('disabled');
         }
     });
 
-    $("#wordSearchButton").click(function () {
+    wordSearchButton.click(function () {
         if (word == '') {
             return;
         }
         var list = $('<table class="pure-table"><thead><tr><th>Etat</th><th>Antall dokumenter som inneholder "' + word + '"</th></tr></thead><tbody></tbody></table>').attr('id', 'wordOwnersList');
-        var wordList = [];
-        var times = owners.length; // counter for API-calls
-        $.each(owners, function(k, owner) {
-            $.getJSON('/api/v4/searchwithname/' + word + '/' + owner, function(data) {
-                var sum = 0;
-                /*
-                    We have to go through all the different kinds of documents and sum the doc_count for each of them to get the total
-                 */
-                $.each(data.toptags, function() {
-                    sum += this.doc_count;
-                });
-                wordList.push({owner: capitalize(owner), sum: sum});
-                times--; // One API-call done.
+        $.getJSON('/api/v4/search/' + word, function(data) {
 
-                /*
-                All API-calls finished.
-                 */
-                if(times == 0) {
-                    wordList = wordList.sort(function(a, b) {
-                        return a.sum < b.sum; // Sort wordList on sum DESC
-                    });
-
-                    /*
-                        Add ordered elements to table.
-                     */
-                    $.each(wordList, function() {
-                        list.append('<tr><td>' + capitalize(this.owner) + '</td><td>' + this.sum + '</td></tr>')
-                    })
-                }
+            if(jQuery.isEmptyObject(data.toptags)) {
+                wordOwnersList.append('<p>Ingen treff</p>');
+                return;
+            }
+            $.each(data.toptags, function(owner) {
+                list.append('<tr><td>' + capitalize(owner) + '</td><td>' + this.doc_count + '</td></tr>');
             })
         });
-        $('#wordOwnersList').empty(); // Clear table to avoid duplication if button is clicked several times
-        $('#wordOwnersList').append(list);
+        wordOwnersList.empty(); // Clear table to avoid duplication if button is clicked several times
+        wordOwnersList.append(list);
     });
 
     /*
-    Prevent default form behavior.
-    Without this, page refreshes on enter.
+     Prevent default form behavior.
+     Without this, page refreshes on enter.
      */
     $("#searchForm").submit(function(e){
         return false;
