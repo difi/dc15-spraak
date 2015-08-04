6abee7ab7c2a7aae3b6ce0f92fbaee7928ca704d
Anna Kastet <annakastet@gmail.com>
2015-08-04 12:34:58 +0200

    Clean up file structure

---
+++ spraak_frontend/public/scripts/ownerTypeahead.js
@@ -0,0 +1,21 @@
+/*
+ Typeahead used in total.html and total_nn.html
+ */
+var loadTypeahead = function() {
+    var owners = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: '/api/v3/owners'
+    });
+
+    $('#ownerSelectTypeahead').typeahead({
+            hint: true,
+            highlight: true,
+            cursor: true,
+            minLength: 1
+        },
+        {
+            name: 'owners',
+            source: owners
+        });
+};
